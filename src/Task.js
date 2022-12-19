7d6cf6e620760f13b0284656c986fd2428c553c3
Andrea Micola Azwir <andreamicolaazwir@gmail.com>
2022-12-19 17:47:19 +0700

    added .gitignore file

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Andrea Micola Azwir" // TODO: replace this
+const studentId = "FE2705553" // TODO: replace this
+
+export { url, studentName, studentId }
