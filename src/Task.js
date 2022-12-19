d80cbd33086b48eef62d5b830653f58c80d89745
Andrea Micola Azwir <andreamicolaazwir@gmail.com>
2022-12-19 18:11:40 +0700

    recover link

--- src/Task.js
+++ src/Task.js
@@ -1,4 +1,4 @@
-const url = "https://resonant-choux-ff4181.netlify.app/add" // TODO: replace this
+const url = "https://resonant-choux-ff4181.netlify.app/" // TODO: replace this
 const studentName = "Andrea Micola Azwir" // TODO: replace this
 const studentId = "FE2705553" // TODO: replace this
 
