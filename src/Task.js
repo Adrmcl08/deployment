de0badbf1d8704c662cdcec05e304c06c3aaf5c9
Andrea Micola Azwir <andreamicolaazwir@gmail.com>
2022-12-19 18:01:26 +0700

    added _redirects file

--- src/Task.js
+++ src/Task.js
@@ -1,4 +1,4 @@
-const url = "YOURDEPLOYURL" // TODO: replace this
+const url = "https://resonant-choux-ff4181.netlify.app/add" // TODO: replace this
 const studentName = "Andrea Micola Azwir" // TODO: replace this
 const studentId = "FE2705553" // TODO: replace this
 
